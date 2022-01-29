4e3e0ff58af27ae0a200cbf4286ec4a7b0ba190e
kaymakanski <49451700+kaymakanski@users.noreply.github.com>
2022-01-29 23:29:04 +0200

    Extracting some modules into SharedModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -33,10 +34,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
